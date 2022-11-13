6249954692057b8f85bdf54e9a4a3054763d41ce
D-Y-So <68039134+D-Y-So@users.noreply.github.com>
2022-11-13 20:37:07 +0200

    created connection singelton

--- src/main/java/ORM_EXE/Client.java
+++ src/main/java/ORM_EXE/Client.java
@@ -1,2 +1,24 @@
-package ORM_EXE;public class Client {
+package ORM_EXE;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Client {
+    public static void main(String[] args) {
+
+        MysqlConnection mysqlcon = MysqlConnection.getInstance();
+        try {
+            Connection con = mysqlcon.getConnection();
+            Statement stmt = con.createStatement();
+            //stmt.executeUpdate("CREATE TABLE AgentDetail (idNo INT(64) NOT NULL AUTO_INCREMENT, initials VARCHAR(2),agentDate DATE, agentCount INT(64),PRIMARY KEY (`idNo`));");
+            //stmt.executeUpdate("DROP TABLE AgentDetail;");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
+    }
 }
