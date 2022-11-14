d2b51199654d033c4b8890512f37fa5ba32299e4
D-Y-So <68039134+D-Y-So@users.noreply.github.com>
2022-11-14 19:07:55 +0200

    added delete table and records feature

---
+++ src/main/java/ORM_EXE/Delete.java
@@ -0,0 +1,59 @@
+package ORM_EXE;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Delete<T> {
+    private final Class<T> clz;
+    private final MysqlConnection con;
+
+    public Delete(Class<T> clz) {
+        this.clz = clz;
+        this.con = MysqlConnection.getInstance();
+    }
+
+    public int deleteTable(){
+        try {
+            int result = this.con.getConnection().createStatement().executeUpdate("DROP TABLE IF EXISTS " + this.clz.getSimpleName().toLowerCase() + ";");
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to create statement while trying to delete table " + this.clz.getSimpleName().toLowerCase() ,e);
+        }
+    }
+
+    public int deleteOneRecordByProperty(String propName, String propValue){
+        if(Validator.isTableExists(con.getConnection(),this.clz.getSimpleName().toLowerCase())==false){
+            System.out.println("Table " + this.clz.getSimpleName().toLowerCase() + " doesn't exist in database. Can't perform delete record by property operation.");
+            return -1;
+        }
+        if(Validator.isFieldExistsInTable(con.getConnection(),this.clz.getSimpleName().toLowerCase(),propName)==false){
+            System.out.println("Column " + propName + " doesn't exist in table " + this.clz.getSimpleName().toLowerCase() + ". Can't perform delete multiple records by property operation.");
+            return -1;
+        }
+        try {
+            int result = this.con.getConnection().createStatement().executeUpdate("DELETE FROM " + this.clz.getSimpleName().toLowerCase() + " WHERE " + propName +  "='" + propValue + "' " + "LIMIT 1;");
+            System.out.println(result + " rows deleted in table " + this.clz.getSimpleName().toLowerCase());
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to create statement while trying to delete row with " + propName + "=" + propValue + "of table " + this.clz.getSimpleName() + ". ",e);
+        }
+    }
+
+    public int deleteAllRecordsByProperty(String propName, String propValue){
+        if(Validator.isTableExists(con.getConnection(),this.clz.getSimpleName().toLowerCase())==false){
+            System.out.println("Table " + this.clz.getSimpleName().toLowerCase() + " doesn't exist in database. Can't perform delete multiple records by property operation.");
+            return -1;
+        }
+        if(Validator.isFieldExistsInTable(con.getConnection(),this.clz.getSimpleName().toLowerCase(),propName)==false){
+            System.out.println("Column " + propName + " doesn't exist in table " + this.clz.getSimpleName().toLowerCase() + ". Can't perform delete multiple records by property operation.");
+            return -1;
+        }
+        try {
+            int result = this.con.getConnection().createStatement().executeUpdate("DELETE FROM " + this.clz.getName().toLowerCase() + " WHERE " + propName +  "='" + propValue + "';");
+            System.out.println(result + " rows deleted in table " + this.clz.getSimpleName().toLowerCase());
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to create statement while trying to delete row with " + propName + "=" + propValue + "of table " + this.clz.getSimpleName() + ". ", e);
+        }
+    }
+}
---
+++ src/main/java/ORM_EXE/Validator.java
@@ -0,0 +1,34 @@
+package ORM_EXE;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Validator {
+    public static boolean isTableExists(Connection con,String tableName){
+        try {
+            DatabaseMetaData md = con.getMetaData();
+            ResultSet rs = md.getTables(null, null, tableName,new String[] {"TABLE"});
+            if(rs.next()==true){
+                return true;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to get Connection's Database Tables. ",e);
+        }
+        return false;
+    }
+
+    public static boolean isFieldExistsInTable(Connection con,String tableName, String fieldName){
+        try {
+            DatabaseMetaData md = con.getMetaData();
+            ResultSet rs = md.getColumns(null, null, tableName, fieldName);
+            if (rs.next()) {
+                return true;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to get table's" + tableName + " columns. ",e);
+        }
+        return false;
+    }
+}
